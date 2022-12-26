40db1a901c4c2a0f9d1058932270cac7e09fc921
Eduardoz18 <eduardoser@gmail.com>
2022-12-26 10:06:33 -0500

    26_Dic_10am

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -11,7 +11,7 @@ import 'rc-slider/assets/index.css'
 export const Home = () => {
     const params = useParams();
     const keyword = params.keyword;
-    const [precio, setPrecio] = useState([100, 1000000])
+    const [precio, setPrecio] = useState([100, 3000000])
     const [currentPage, setCurrentPage] = useState(1)
     const { loading, products, error, resPerPage, productsCount } = useSelector(state => state.products)
     const alert = useAlert();
